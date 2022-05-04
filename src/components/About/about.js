97b9c9a4245e30f041b080430b6f9126c4653b07
Alexandre Bourdeaud'Hui <a.bourdeaudhui@mamoka.com>
2022-05-04 10:47:14 +0200

    Semplificazione gestione dati film

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -9,51 +9,23 @@ export class About {
     this.router = router;
     this.heading = 'Movie';
     this.subheading = 'Ecco la lista dei film. Cliccando sul tasto corrispettivo, potrai modificare i dati';
+  }
 
+  attached() {
     let token = window.localStorage.getItem('token');
-
-    //console.log(token);
-
     fetch('http://kamaji2.dev.netbuilder.it/00900001/movies',{
       method: 'GET',
       headers: {
-      Authorization: `Bearer ${token}`,
-      },   
+        Authorization: `Bearer ${token}`,
+      }
     })
-    .then(response=>response.json())
-    .then(data=>{
-      //console.log(data);
+    .then(response => response.json())
+    .then(data => {
       this.data = data;
     })
   }
 
   updateContent(data) {
-    console.log(data.id);
-    let token = window.localStorage.getItem('token');
-    fetch(`http://kamaji2.dev.netbuilder.it/00900001/movies/${data.id}`,{
-      method: 'GET',
-      headers: {
-        'Content-Type': 'raw',
-        Authorization: `Bearer ${token}`,
-      },
-      
-    })
-    .then(res=>res.json())
-    .then(data=>{
-      //console.log(data);
-      this.router.navigateToRoute(`edit`);
-
-      window.localStorage.setItem(
-        'movieData', JSON.stringify({
-          'title' : data.title,
-          'director' : data.director,
-          'year' : data.year
-        }),                
-      )
-      window.localStorage.setItem(        
-        'movieId', data.id
-      )     
-    }) 
-    
+    this.router.navigateToRoute('edit', { id: data.id });
   }
 }
--- src/components/Edit/edit.js
+++ src/components/Edit/edit.js
@@ -7,37 +7,47 @@ export class edit{
   constructor(router) {
     this.router = router;
     this.heading = 'Edit';
-    this.subheading = 'Scegli il campo da modificare e poi clicca sul bottone.';    
-    
-    let moviDat = JSON.parse(localStorage.movieData);
-    
-    this.title = moviDat.title;
-    this.director = moviDat.director;
-    this.year = moviDat.year;
-    this.id = localStorage.movieId;          
+    this.subheading = 'Scegli il campo da modificare e poi clicca sul bottone.';
+  }
+
+  activate(params) {
+    this.params = params;
+  }
+
+  attached() {
+    if (this.params.id) {
+      let token = window.localStorage.getItem('token');
+      fetch(`http://kamaji2.dev.netbuilder.it/00900001/movies/${this.params.id}`,{
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.data = data;
+      }) 
+    }
   }
   
   update(title,id) {
-    let token = window.localStorage.getItem('token');   
-    
-    fetch(`http://kamaji2.dev.netbuilder.it/00900001/movies/${localStorage.movieId}`,{
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'raw',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      "title": title,
-      "director" : this.director,
-      "year" : this.year        
+    let token = window.localStorage.getItem('token');
+    fetch(`http://kamaji2.dev.netbuilder.it/00900001/movies/${this.params.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        title: this.data.title,
+        director: this.data.director,
+        year: this.data.year
+      })
     })
-  })
-  .then((res)=>res.json())
-  .then((data)=>{
-    //console.log(data);
-    this.router.navigateToRoute(`about`);
-    localStorage.removeItem('movieData');
-    localStorage.removeItem('movieId');      
-  }); 
-}
+    .then((res) => res.json())
+    .then((data) => {
+      this.router.navigateToRoute(`about`);
+    }); 
+  }
 }
